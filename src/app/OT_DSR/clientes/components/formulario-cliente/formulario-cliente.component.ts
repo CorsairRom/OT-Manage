f588c83841c8a9065e9c005f889da53f971b3b26
CorsairRom <ric.romero@duocuc.cl>
2023-07-23 17:12:25 -0400

    finish form cliente

--- src/app/OT_DSR/clientes/components/formulario-cliente/formulario-cliente.component.ts
+++ src/app/OT_DSR/clientes/components/formulario-cliente/formulario-cliente.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
-import { ClienteForm } from '../../interfaces/clientes.interface';
+import { ClienteForm, ClienteResponse } from '../../interfaces/clientes.interface';
 import { FormBuilder, Validators } from '@angular/forms';
 
 
@@ -11,17 +11,25 @@ import { FormBuilder, Validators } from '@angular/forms';
 
 })
 export class FormularioClienteComponent {
-  @Input() cliente? : ClienteForm
+  @Input() cliente? : ClienteResponse
   @Input() title? : string
 
   @Output() submitEvent = new EventEmitter<ClienteForm>();
   @Output() cancelEvent = new EventEmitter<void>();
 
+  opcionesPais = [
+     // Opción por defecto (null)
+    { id: 45, nombre: 'Chile' }, // Opción con id 45 (Chile)
+    // Otras opciones de países si las tienes
+  ];
+
+  componentComunaRegion: boolean = false;
+
   fb = inject(FormBuilder)
 
   form = this.fb.group({
-    comuna: this.fb.nonNullable.control<string>('', [Validators.required, Validators.maxLength(50)]),
-    pais: this.fb.control<number | null >(null, []),
+    comuna_id: this.fb.nonNullable.control<number | null>(null, [Validators.required, Validators.maxLength(50)]),
+    pais: this.fb.control<number | null >(null, [Validators.required]),
     razon_social: this.fb.nonNullable.control<string>('', [Validators.required, Validators.maxLength(50)]),
     rut: this.fb.nonNullable.control<string>('',
       [
@@ -32,9 +40,41 @@ export class FormularioClienteComponent {
     sitio_web: this.fb.control<string | null>(null, []),
     telefono: this.fb.nonNullable.control<number>(0, [Validators.required]),
     direccion: this.fb.nonNullable.control<string>('', [Validators.required, Validators.maxLength(240)]),
-    codigo_postal: this.fb.control<string | null>(null, [])
+    codigo_postal: this.fb.control<number | null>(null, [])
   });
 
+  handleSelectedComuna(comunaId: number | null) {
+    this.form.patchValue({ comuna_id: comunaId })
+  }
 
+  onPaisSelect(event: any) {
+    if (event.value.id === 45) {
+      this.form.patchValue({pais: event.value.id})
+      this.componentComunaRegion = true;
+    }
+  }
+
+  submit(){
+    if(this.form.invalid) return;
+
+    const values = this.form.getRawValue();
+    const clienteForm:ClienteForm  = {
+      comuna_id: values.comuna_id!,
+      pais: values.pais,
+      razon_social: values.razon_social,
+      rut: values.rut,
+      sitio_web: values.sitio_web,
+      telefono: values.telefono,
+      direccion: values.direccion,
+      codigo_postal: values.codigo_postal!
+    }
+
+
+    this.submitEvent.emit(clienteForm);
+  }
+
+  cancelar() {
+    this.cancelEvent.emit();
+  }
 
 }
