06158a2a69c86c81147bb3fdc33794d05dc673d3
CorsairRom <ric.romero@duocuc.cl>
2023-08-16 01:37:31 -0400

    update tecnico-ot component

--- src/app/OT_DSR/ordenes-trabajo/components/tecnico-ot/tecnico-ot.component.ts
+++ src/app/OT_DSR/ordenes-trabajo/components/tecnico-ot/tecnico-ot.component.ts
@@ -1,4 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, Pipe, inject } from '@angular/core';
+import { UsuariosResponse } from 'src/app/OT_DSR/administrar-accesos/usuarios/interfaces/usuario.interface';
+import { UsuariosService } from 'src/app/OT_DSR/administrar-accesos/usuarios/services/usuarios.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'tecnico-ot',
@@ -6,8 +9,15 @@ import { Component, Input } from '@angular/core';
   styles: [
   ]
 })
-export class TecnicoOTComponent {
+export class TecnicoOTComponent implements OnInit {
 
+  private usuarioService = inject(UsuariosService);
 
+  tecnicos?: UsuariosResponse[];
 
+  ngOnInit(): void {
+    this.usuarioService.getUsuarios().subscribe(
+     u => this.tecnicos = u
+    )
+  }
 }
