32f7ac18b4d3267b2f879eb38cc43016b22ced1a
CorsairRom <ric.romero@duocuc.cl>
2023-08-23 18:41:04 -0400

    add handle error and getInforme_ot

--- src/app/OT_DSR/ordenes-trabajo/services/informe-ot.service.ts
+++ src/app/OT_DSR/ordenes-trabajo/services/informe-ot.service.ts
@@ -1,7 +1,9 @@
 import { Injectable, inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { MensajeService } from '../../core/services/message.service';
 import { environment } from 'src/environments/environment';
+import { BehaviorSubject, catchError, tap, throwError } from 'rxjs';
+import { informeOTResponse } from '../interfaces/informe-ot.interface';
 
 @Injectable({providedIn: 'root'})
 export class InformeOTService {
@@ -10,7 +12,27 @@ export class InformeOTService {
   private messageService = inject(MensajeService);
   private apiUrl = `${ environment.apiUrl }/api/informeOT`;
 
+  private otSubject = new BehaviorSubject<informeOTResponse[]>([]);
+  informeOT$ = this.otSubject.asObservable();
 
+  private handleError(error: HttpErrorResponse) {
+    const msg = JSON.stringify(error.error);
+    if (error.status == 400 || error.status == 404) {
+        const errores = Object.entries(error.error).map((msg) =>`${msg[0].toUpperCase()}: ${msg[1]}`);
+        this.messageService.addMessage({
+            details: errores,
+            role: 'error',
+        });
+    }
+    return throwError(() => new Error(msg));
+  };
 
+  getInforme_OT(){
+    return this.http.get<informeOTResponse[]>(`${this.apiUrl}/`).pipe(
+      // inicializar behaviorSubject con los datos obtenidos
+      tap(infoOT => this.otSubject.next(infoOT)),
+      catchError((err: HttpErrorResponse) => this.handleError(err))
+    );
+  };
 
 }
