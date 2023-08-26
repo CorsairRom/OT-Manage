c62d1ebe9dfef1cd55bcbd9965696ea03a175163
CorsairRom <ric.romero@duocuc.cl>
2023-08-26 10:54:48 -0400

    update env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-    production: true,
+    production: false,
     apiUrl: 'http://127.0.0.1:8000',
 };
 
