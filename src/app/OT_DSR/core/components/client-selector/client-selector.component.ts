0e6fb74bcf2c18de53e81661347f9f3a1c5ebade
CorsairRom <ric.romero@duocuc.cl>
2023-08-28 09:25:19 -0400

    update registroOT

--- src/app/OT_DSR/core/components/client-selector/client-selector.component.ts
+++ src/app/OT_DSR/core/components/client-selector/client-selector.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Input, OnInit, Output, inject } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, inject } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { ClienteRES } from 'src/app/OT_DSR/clientes/interfaces/clientes.interface';
 import { ClientesService } from 'src/app/OT_DSR/clientes/services/clientes.service';
 
@@ -14,25 +15,28 @@ export class ClientSelectorComponent implements OnInit {
   @Input() initialClientId?: number | null;
   @Output() selectedClientIdEvent = new EventEmitter<number | null>();
   @Output() createClientEvent = new EventEmitter<boolean>();
+  @Output() initialClientIdChange = new EventEmitter<number>();
 
   currentClient?: ClienteRES | null = null;
   clients$: ClienteRES[] = [];
-
   private clientService = inject(ClientesService);
+
 
   ngOnInit(): void {
     this.clientService.getClientes().subscribe();
     this.clientService.clientes$.subscribe(
       res => this.clients$ = res
     )
+    this.clientService.newCliente$.subscribe(
+      newCliente => {
+        if (newCliente) {
+          this.currentClient = newCliente;
+        }
+      }
+    )
 
     if(this.initialClientId) {
-      // this.clientService.getClienteById(this.initialClientId).subscribe(client => this.currentClient = client);
-      this.clientService.clientes$.subscribe(res => {
-        this.currentClient = res.find(c => c.id === this.initialClientId)
-        console.log(this.currentClient);
-        console.log(res);
-      })
+      this.clientService.getClienteById(this.initialClientId).subscribe(client => this.currentClient = client);
     }
   }
 
