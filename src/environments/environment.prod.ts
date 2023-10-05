9c857ced7d92157dfee8d6fad94522651d94c5ab
CorsairRom <ric.romero@duocuc.cl>
2023-10-04 21:08:51 -0300

    change enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://acis-production.up.railway.app'
+    // apiUrl: 'https://acis-production.up.railway.app'
+    apiUrl: 'http://192.168.1.81:8000'
 };
